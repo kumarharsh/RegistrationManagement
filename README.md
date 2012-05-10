5a8c6763cdaaf272182d1ba512cd4c38e76dc388
Kumar Harsh <harsh.manutdblu@gmail.com>
2012-05-10 15:34:39 +0530

    Updated rules.php, deleted dummy pages, rewrote the readme file

--- README.md
+++ README.md
@@ -7,10 +7,42 @@ when registering for a new semester at his University.
 Additionally, an option is given to him to be able to 
 Add/Drop his courses.
 
+
+##Installation Instructions
+### Database 
+For setting up the database for the project,
+install "PHPMyAdmin" software, which will allow you to
+easily see the database structure.
+*You can also use the normal mysql command-line program for this*
+
+* Now, Create a database named 'dbms'
+* Now, Use the newly created 'dbms' database
+* Import the mysql schema from the file **database/schema.sql**
+
+### Web-Application
+To use the Registration Management application, unzip the folder, and 
+copy the contents into your Apache web directory
+* */var/www/* for linux
+* *C:/wamp/www/* for windows
+Now browse to the index.php file using your browser.
+
+And you are good to go.
+
+The database has been pre-populated with some default users and courses.
+For using the application, some useful data are:
+
+#### Login as Dean
+*username:*dean
+*password:*dean
+
+#### Login as Student
+*username:*y09uc001 to y0ouc005
+*password:*y09uc001 to y09uc005
+
+
 ##To Do
-###Dummy Database Entries
-A database of dummy students has to be created and filled for usage. Also
-dummy courses have to be generated to simulate the working.
-
-###Generate TestCases and do Unit and System testing
-
+### Implemented multiple course registrations at one time
+Replace the current radio-button type registration with a checkbox-type 
+registration, so that a user can check all the courses he wants to register
+in one go. The system should notify the user on a per-course basis whether 
+his registration was successful or not.
---
+++ rules.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!-- Consider adding an manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 8]><!-->
+<html id="courseReg" class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+
+  <!-- Use the .htaccess and remove these lines to avoid edge case issues.
+       More info: h5bp.com/b/378 -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>LNMIIT Course Registration</title>
+  <meta name="description" content="This is a module for course registration to be used by the institute administration to offer the students with an easy and intutive way of registering for their courses."/>
+  <meta name="author" content="Kumar Harsh, Anoop Malav, Pankaj Singhal">
+
+  <!-- Mobile viewport optimized: j.mp/bplateviewport -->
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+
+  <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
+  <link rel="shortcut icon" type="image/ico" href="favicon.ico" />
+ 
+  <!-- include the stylesheets to be used on the page --> 
+  <?php include("css/main.css"); ?>
+  <link rel="stylesheet" href="css/navbar.css"/>
+  <link rel="stylesheet" href="css/nivo-slider.css"/>
+  <link rel="stylesheet" href="css/nivo-default-theme.css"/>
+  <!-- end CSS-->
+
+
+  <!-- All JavaScript at the bottom, except for Modernizr / Respond.
+       Modernizr enables HTML5 elements & feature detects; Respond is a polyfill for min/max-width CSS3 Media Queries
+       For optimal performance, use a custom Modernizr build: www.modernizr.com/download/ -->
+  <script src="js/modernizr-2.0.6.min.js"></script>
+</head>
+
+<body>
+  <div id="wrapper">
+
+    <header>
+      <div class="container_24"> 
+        <?php include('widgets/header.php'); ?>
+        <?php include('widgets/topnav.php'); ?>
+      </div>
+    </header>
+
+    <div id="main" class="container_24" role="main">
+
+      <section id="content">
+        <h1>Rules</h1>
+        <p>No rules are defined.</p>
+      </section>
+
+
+      <footer>
+         <?php include("widgets/footer.php"); ?> 
+      </footer>
+    </div>
+  </div>
+
+
+  <!-- JavaScript at the bottom for fast page loading -->
+  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+<!-- 
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.2.min.js"><\/script>')</script>
+ -->
+  <script type="text/javascript" src="js/modernizr-2.0.6.min.js"></script>
+  <script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
+  <script type="text/javascript" src="js/jquery-ui-1.8.18.min.js"></script>
+  <script type="text/javascript" src="js/jquery.nivo.slider.pack.js"></script>
+  <script type="text/javascript">
+    $(window).load(function() {
+        $('#slider').nivoSlider();
+        $('#login-form').submit(function(e) {
+            e.preventDefault();
+            var postUrl = $(this).attr("action");
+            var request = $.ajax({
+                type: 'POST',
+                url: postUrl,
+                data: $(this).serialize()
+            })
+            request.done(function(response) {
+                if(response.status==="success") {
+                    window.location.href = response.access;
+                }
+                else {
+                    $('.form-error').show().html(response.message);
+                }            
+            });
+        });
+    });
+  </script>
+  <!-- end scripts-->
+	
+
+  <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you want to support IE 6.
+       chromium.org/developers/how-tos/chrome-frame-getting-started -->
+  <!--[if lt IE 7 ]>
+    <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+    <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+  <![endif]-->
+
+</body>
+</html>
